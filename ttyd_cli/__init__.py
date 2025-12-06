6fa0e3599111745c69742e411ad8ea5c3d8a2092
krypton-byte <pujasuara42@smk.belajar.id>
2025-12-06 13:58:13 +0700

    fix: versioning

--- ttyd_cli/__init__.py
+++ ttyd_cli/__init__.py
@@ -8,9 +8,6 @@ from importlib.metadata import PackageNotFoundError, version
 from .client import TTYDClient
 from .exceptions import InvalidAuthorization
 
-try:
-    __version__ = version("ttyd-client")
-except PackageNotFoundError:
-    __version__ = "0.0.0.dev0"
+__version__ = "0.1.0"
 
 __all__ = ["TTYDClient", "InvalidAuthorization"]
